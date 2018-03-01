9ef0dd2f7a42b73cd523d0ff6396dd2deacf6aed
Dmitry Abramenko <abramenko@a2design.biz>
2018-03-01 17:41:30 +0600

    initial commit

---
+++ resources/assets/js/index.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+
+import App from './components/App';
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(<App/>, rootEl);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+
+                <div id="root">
+                    <!-- React app -->
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
